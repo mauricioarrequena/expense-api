957f4c408e3a41917d39b2fdd94f823cdec5c421
unknown <mauricio.ar.requena@gmail.com>
2025-04-19 15:58:16 -0400

    feat:added account entity and added typeorm, postgree packages

---
+++ src/models/Account .ts
@@ -0,0 +1,18 @@
+// src/entities/Account.ts
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class Account {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column()
+  accountName!: string;
+
+  @Column()
+  balance!: number;
+
+  @Column()
+  accountType!: string; // You can change this to match your schema
+}
+
